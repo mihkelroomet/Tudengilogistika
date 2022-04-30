351365bffd02a9045354e13f05526693ea0026e0
mihkelroomet <60692429+mihkelroomet@users.noreply.github.com>
2022-04-30 15:44:25 +0300

    Add files via upload

---
+++ lihtne_päring_1.sql
@@ -0,0 +1,4 @@
+// Leiame kõik bussiliinid, mis läbivad peatust 'Kesklinn' või peatust 'Soola'
+SELECT f_bussiliini_nimi(Bussiliinid.Bussiliinid_ID) AS Bussiliin
+FROM Bussiliinid KEY JOIN Bussiliinid_Bussipeatused KEY JOIN Bussipeatused
+WHERE Bussipeatused_Nimi = 'Kesklinn' OR Bussipeatused_Nimi = 'Soola';
---
+++ lihtne_päring_2.sql
@@ -0,0 +1,5 @@
+// Leiame kõik järjestikused peatused, mida läbib liin nr 2 suunal Lõunakeskus
+SELECT Bussipeatused_Nimi AS Bussipeatus
+FROM Bussipeatused KEY JOIN Bussiliinid_Bussipeatused KEY JOIN Bussiliinid
+WHERE Bussiliinid_Number = 2 AND Bussiliinid_Sihtpeatus = 'Lõunakeskus'
+ORDER BY Bussipeatuse_Järjekorranumber;
